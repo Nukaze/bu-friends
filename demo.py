168fa76b6e2be939e6c55723aefcbf8f2282a444
Nukaze <nukaze4@gmail.com>
2022-03-01 18:13:29 +0700

    Complete 70% of Register Section and add a little title

--- demo.py
+++ demo.py
@@ -1,8 +1,13 @@
 '''
     demo BUFriends
 '''
+from atexit import register
+from distutils.log import Log
+from doctest import master
+from multiprocessing.context import set_spawning_popen
 from tkinter import *
 from tkinter import ttk, messagebox
+from unittest import case
 from PIL import Image,ImageTk
 import os
 import hashlib
@@ -43,6 +48,8 @@ class LogIn(Frame):
     class LogInContent:
         def __init__(self, root, masterFrame):
             self.bg,self.fg = "#ccefff","#cc07e6"
+            self.masterFrame = masterFrame
+            self.masterFrame.title("BU Friends  |  Log-In")
             self.fontHead = "Kanit 36 bold"
             self.font = "Kanit 16 "
             Label(root,text="BU Friends  |  Log-In",font=self.fontHead,bg=self.bg,foreground=self.fg,justify="left")\
@@ -92,8 +99,9 @@ class Registration(Frame):
     class RegisterContent:
 
         def __init__(self, root, masterFrame):
-            self.masterFrame = masterFrame
             self.bg,self.fg = "#ccefff","#cc07e6"
+            self.masterFrame = masterFrame
+            self.masterFrame.title("BU Friends  |  Registration")
             self.fontHead = "Kanit 36 bold"
             self.font = "Kanit 16 "
             Label(root, text="BU Friends  |  Registration",font=self.fontHead,bg=self.bg,foreground=self.fg)\
@@ -111,7 +119,7 @@ class Registration(Frame):
             self.regisBtn = Button(self.frameBtn,text="Register!",command=self.regis_submit,relief="solid",width=30,height=3
                                    ,bg="#edffbf")
             self.regisBtn.grid(row=4,column=1,sticky="nsew",padx=2,pady=15)
-            self.CancelBtn = Button(self.frameBtn,text="Cancel",command=lambda :masterFrame.switch_page(LogIn),relief="solid",width=20,height=2)
+            self.CancelBtn = Button(self.frameBtn,text="Cancel",command=lambda :self.masterFrame.switch_page(LogIn),relief="solid",width=20,height=2)
             self.CancelBtn.grid(row=4,column=2,sticky="nsew",padx=2,pady=15)
 
         def regis_form(self,_text,_font,_row,_entVar):
@@ -125,48 +133,51 @@ class Registration(Frame):
                         .grid(row=_row, column=2, sticky="nsew",padx=5, pady=10,ipadx=80,ipady=2)
 
         def regis_submit(self): 
-
-            def register_error(errorFormat):
-                #self.regisVarData.clear()
+            def register_error(errorFormat=""):
+                self.regisVarData.clear()
                 self.regisDataSubmit.clear()
                 messagebox.showinfo('Register Error', '{}\nPlease Register Form Again'.format(errorFormat))
                 self.masterFrame.switch_page(Registration)
-
             def register_validate(self):
+                for i,data in enumerate(self.regisVarData):
+                    if data.get() == "" or data.get().isspace():
+                        register_error("Register Form Information not Complete")
+                        break
+                register_email_validate(self)
+
+            def register_email_validate(self):
                 dbUserDataLst = []
                 with open('database/user.txt',buffering=1)as db:
-                    while True:
+                    while db:
                         dbdata = db.readline().split()
                         if dbdata == []:break
                         else:dbUserDataLst.append(dbdata)
-                        dbUserDataLst[1]
                     print(dbUserDataLst)
                     for linedata in dbUserDataLst:
-                        if self.regisVarData[0].get() == linedata[0]:
-                            register_error("{} is Existed".format(self.regisVarData[0].get()))
-                            print("end")
+                        if "@bumail.net" not in self.regisVarData[0].get():
+                            register_error("BU Friends Exclusive for BU Mail only")
                             break
+                        elif self.regisVarData[0].get() == linedata[0]:
+                            register_error("{} is Existed".format(self.regisVarData[0].get()))
+                            break
+                    register_password_validate(self)
+                        
+            def register_password_validate(self):
                 for i,data in enumerate(self.regisVarData):
-                    if data.get() == "" or data.get().isspace():
-                        self.regisVarData.clear()
-                        register_error("Register Form Information not Complete")
-                        break
-                    if "@bumail.net" not in self.regisVarData[0].get():
-                        self.regisVarData.clear()
-                        register_error("BU Friends Exclusive for BU Mail only")
-                        break
                     if self.regisVarData[2].get() != self.regisVarData[3].get():
                         register_error("Password is not Matching")
                         break
-                    else:
-                        self.regisDataSubmit.append(data.get())
-                if len(self.regisVarData) == len(self.regisVarData):
-                    with open('database/user.txt', 'a+')as db:
-                        db.writelines("{} {} {} {}\n".format(*self.regisDataSubmit))
-                        messagebox.showinfo('Register Successfully'
-                                            ,"BUMail : {} DisplayName : {}\nPassword1 : {}\nPassword2 : {}".format(*self.regisDataSubmit))
-                else:
-                    register_error("dunno u forgot")
+                    else:self.regisDataSubmit.append(data.get())
+                register_confirm(self)
+                
+            def register_confirm(self):
+                print(self.regisDataSubmit)
+                with open('database/user.txt', 'a+')as db:
+                    db.writelines("{} {} {} {}\n".format(*self.regisDataSubmit))
+                    messagebox.showinfo('Register Successfully'
+                                        ,"BUMail : {} DisplayName : {}\nPassword1 : {}\nPassword2 : {}".format(*self.regisDataSubmit))
+                    messagebox.showinfo('Redirecting',"Going to BU Friends | Log-in")
+                    self.masterFrame.switch_page(LogIn)
             register_validate(self)
 
 class DashBoard(Frame):
@@ -183,6 +194,7 @@ class DashBoard(Frame):
         def __init__(self, root, masterFrame):
             x = 10
             self.masterFrame = masterFrame
+            self.masterFrame.title("BU Friends  |  Dashboard")
             self.fontHead = "Kanit 48 bold"
             self.font = "Kanit 16"
             Label(root, text="Dashboard",font=self.fontHead).pack()
