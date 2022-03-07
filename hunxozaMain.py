f3857257a222bc76729d6d7df781b1052aea9ef7
Hunxoza <mr.mataxdandie@gmail.com>
2022-03-07 23:41:13 +0700

    update

--- hunxozaMain.py
+++ hunxozaMain.py
@@ -1,33 +1,108 @@
 import sqlite3
 from sqlite3 import Error
+from tkinter import *
+from tkinter.font import Font
 
-class dbController() :
+# connecting to database
+class DBController() :
     def create_connection():
         conn = None
         try:
-            conn = sqlite3.connect(r"./database/BUFriend.db")
+            conn = sqlite3.connect(r"./database/BUFriends.db")
+            conn.execute("PRAGMA foreign_keys = 1")
             print(sqlite3.version)
         except Error as e:
             print(e)
         return conn
 
-    def create_table(conn, create_table_sql):
+    def execute_sql(conn, sql):
         try:
             c = conn.cursor()
-            c.execute(create_table_sql)
+            c.execute(sql)
+            conn.commit()
         except Error as e:
             print(e)
+class BUFriends(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.frame = None
+        screenwidth = self.winfo_screenwidth()
+        screenheight = self.winfo_screenheight()
+        w,h = 900,600
+        x = (screenwidth//2)-(w//2)
+        y = (screenheight//2-50)-(h//2)
+        self.geometry("{}x{}+{}+{}".format(w,h,x,y))
+        self.iconbitmap("assets/icons/bufriends.ico")
+        self.resizable(0,0)
+        self.fontHead = Font(family="leelawadee",size=36,weight="bold")
+        self.switch_frame(ProfilePage)
+# switch page event
+    def switch_frame(self, frameClass):
+        new_frame = frameClass(self)
+        if self.frame is not None:
+            self.frame.destroy()
+        self.frame = new_frame
+        self.configure(bg = self.frame.bgColor)
+        self.frame.pack(side=BOTTOM, fill=BOTH, expand=TRUE)
+class ScrollFrame():
+    def __init__(self,root,scrollable):
+        # creating
+        self.root = root
+        self.scrollable = scrollable
+        self.scrollbar = Scrollbar(self.root, orient=VERTICAL,width=0)
+        self.scrollbar.pack(fill=Y, side=RIGHT, expand=0)
+        self.canvas = Canvas(self.root, highlightthickness=0, yscrollcommand=self.scrollbar.set)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=1)
+        self.scrollbar.config(command=self.canvas.yview)
+        # reset the view
+        self.canvas.xview_moveto(0)
+        self.canvas.yview_moveto(0)
 
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = Frame(self.canvas)
+        self.interior_id = self.canvas.create_window(0, 0, window=self.interior,anchor=NW)
+
+        self.interior.bind('<Configure>', self._configure_interior)
+        self.canvas.bind('<Configure>', self._configure_canvas)
+        self.canvas.bind('<Enter>', self._bind_to_mousewheel)
+        self.canvas.bind('<Leave>', self._unbind_from_mousewheel)
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+
+    def _configure_interior(self, event):
+        # update the scrollbars to match the size of the inner frame
+        size = (self.interior.winfo_reqwidth(), self.interior.winfo_reqheight())
+        self.canvas.config(scrollregion="0 0 %s %s" % size)
+
+        if self.interior.winfo_reqwidth() != self.root.winfo_width():
+            # update the canvas's width to fit the inner frame
+            self.canvas.config(width=self.interior.winfo_reqwidth())
+
+    def _configure_canvas(self, event):
+        if self.interior.winfo_reqwidth() != self.root.winfo_width():
+            # update the inner frame's width to fill the canvas
+            self.canvas.itemconfigure(self.interior_id, width=self.root.winfo_width())
+
+    # This can now handle either windows or linux platforms
+    def _on_mousewheel(self, event):
+        if self.scrollable == TRUE :
+            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        else :
+            self.canvas.yview_scroll(0, "units")
+    def _bind_to_mousewheel(self, event):
+        self.root.bind_all("<MouseWheel>", self._on_mousewheel)
+
+    def _unbind_from_mousewheel(self, event):
+        self.root.unbind_all("<MouseWheel>")
+class ProfilePage(Frame):
+    def __init__(self,controller):
+        Frame.__init__(self,controller)
+        self.bgColor = 'lightblue'
+        self.controller = controller
+        Frame.configure(self,bg=self.bgColor)
+        scroll = ScrollFrame(self,FALSE)
+        master = scroll.interior
+        Label(master,text="Registrati",font=controller.fontHead).pack()
 if __name__ == '__main__':
-    sql = """ CREATE TABLE IF NOT EXISTS projects (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    begin_date text,
-                                    end_date text
-                                ); """
-    conn = dbController.create_connection()
-    if conn is not None:
-            # create projects table
-            dbController.create_table(conn, sql)
-    else:
-        print("Error! cannot create the database connection.")
+    app = BUFriends()
+    app.mainloop()
