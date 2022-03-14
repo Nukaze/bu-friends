a5287f481a7632be1e7653dcd65d1aaadf9fe7c9
Nukaze <nukaze4@gmail.com>
2022-03-14 19:52:25 +0700

    add mbtiData

---
+++ assets/mbti/mbtiData.py
@@ -0,0 +1,103 @@
+import random
+def get_MbtiQuizEN():
+    return [["IE","Expend energy, enjoy groups or b. conserve energy, enjoy one-on-one"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            
+            ["PJ","WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER?"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"]    
+]
+def get_MbtiQuizTH():
+     return [["IE","คูณชื่นชอบการอยู่กับกลุ่มเพื่อนแบบไหน ?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            ["IE","Are u out-going?"],
+            
+            ["NS","ถ้าหากว่าวันนึงคุณได้เป็นอาจารย์ คุณอยากสอนนักเรียนแบบไหน?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            ["NS","Which one you trust Present or Future?"],
+            
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            ["FT","When someone make mistake what you do to help?"],
+            
+            ["PJ","WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER?"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"],
+            ["PJ","Have you wake-up instanly when alarm is clocking"]    
+]
+    
+def get_MbtiAnsTH():
+    return [["I","E","เพลิดเพลินกับเพื่อน ๆ กลุ่มใหญ่","เพลิดเพลินกับเพื่อน ๆ กลุ่มเล็ก"],
+            ["I","E","2 Are u out-going?",""],
+            ["I","E","3 Are u out-going?",""],
+            ["I","E","4 Are u out-going?",""],
+            ["I","E","5 Are u out-going?",""],
+            ["I","E","6 Are u out-going?",""],
+            ["I","E","7 Are u out-going?",""],
+            
+            ["S","N","สอนตามหลักสูตรตามข้อเท็จจริงที่มีการพิสูจน์แล้ว","หลักสูตรที่เกี่ยวกับหลักการ แนวคิด ทฤษฎี"],
+            ["N","S","",""],
+            ["N","S","",""],
+            ["N","S","",""],
+            ["N","S","",""],
+            ["N","S","",""],
+            ["N","S","",""],
+            
+            ["F","T","",""],
+            ["F","T","",""],
+            ["F","T","",""],
+            ["F","T","",""],
+            ["F","T","",""],
+            ["F","T","",""],
+            ["F","T","",""],
+            
+            ["J","P","งั้นเรามาวางแผนการเที่ยวกันก่อนว่าทำอะไร วันไหน","ไปกันเลย ลุย!!"],
+            ["P","J","",""],
+            ["P","J","",""],
+            ["P","J","",""],
+            ["P","J","",""],
+            ["P","J","",""],
+            ["P","J","",""]    
+]
+
+#randLst = random.sample(range(len(quiz)),len(quiz))
